17d2f945e42912d992fe74c3c8a97bf817e9f264
jkuehner <jochen.kuehner@kardex.com>
2015-08-15 00:42:28 +0200

    - Nuget-Package - Include Code from Codplex WpfGauge (also in SL)

--- Codeplex.Dashboarding/AssemblyVersion.cs
+++ Codeplex.Dashboarding/AssemblyVersion.cs
@@ -27,14 +27,13 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.6.1.0")]
+[assembly: AssemblyVersion("2.0.*")]
 
-[assembly: AssemblyFileVersion("0.6.1.0")]
-[assembly: AssemblyInformationalVersion("Release 009")]
+[assembly: AssemblyFileVersion("2.0.*")]                   
 
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("David black")]
-[assembly: AssemblyProduct("Codeplex.Dashboarding")]
+[assembly: AssemblyCompany("DotNetProjects")]
+[assembly: AssemblyProduct("DotNetProjects.Dashboarding")]
 [assembly: AssemblyCopyright("Copyright © David Black 2008 - 2009")]
 [assembly: AssemblyTrademark("")]
 
